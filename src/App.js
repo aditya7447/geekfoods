e2beecf91085cf4b14efa3af6f212ce33ff4b19a
unknown <adityawakarekar9604@gmail.com>
2024-03-25 19:32:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import HeroSection from './Components/HeroSection';
+import Button from './Components/Button';
+import Footer from './Components/Footer';
+import Cards from './Components/Cards';
+import Images from './Components/Images';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Header/>
+        <HeroSection/>
+        <Button/>
+        <Footer/>
+        <Cards/>
+        <Images/>
+      </div>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <div>
+            <h1 className="helloText">Hello World</h1>
+        </div>
+    );
+}
+
+export default Header;
